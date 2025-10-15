758a281c992d4fc7e1cf37d9d57d723794df7b9e
thesmartshadow <firaswq12@gmail.com>
2025-10-15 03:18:32 +0300

    Create Alert.tsx

---
+++ components/ui/Alert.tsx
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import { cn } from '../../lib/utils';
+
+const alertVariants = {
+  variant: {
+    default: 'bg-background text-foreground',
+    destructive: 'border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive',
+    success: 'border-success/50 text-success dark:border-success [&>svg]:text-success'
+  },
+};
+
+const Alert = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement> & { variant?: keyof typeof alertVariants.variant }>(
+  ({ className, variant = 'default', ...props }, ref) => (
+    <div
+      ref={ref}
+      role="alert"
+      className={cn(
+        'relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
+        alertVariants.variant[variant],
+        className
+      )}
+      {...props}
+    />
+  )
+);
+Alert.displayName = 'Alert';
+
+const AlertTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
+  ({ className, ...props }, ref) => (
+    <h5
+      ref={ref}
+      className={cn('mb-1 font-medium leading-none tracking-tight', className)}
+      {...props}
+    />
+  )
+);
+AlertTitle.displayName = 'AlertTitle';
+
+const AlertDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn('text-sm [&_p]:leading-relaxed', className)}
+      {...props}
+    />
+  )
+);
+AlertDescription.displayName = 'AlertDescription';
+
+export { Alert, AlertTitle, AlertDescription };
